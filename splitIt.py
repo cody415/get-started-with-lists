f3552067a9978e7b3b84ead86d4ee693b201250c
Aparna1407 <aparnadesai7@gmail.com>
2024-09-29 00:49:52 +0530

    115

---
+++ splitIt.py
@@ -0,0 +1,20 @@
+# Define a list of integers from 1 to 10
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Slice the list to get every second element, starting from the beginning
+sublist = my_list[::2]
+
+# Print the sliced sublist containing every second element
+print(sublist) 
+
+# Slice the list to get every second element, starting from index 1
+sublist = my_list[1::2]
+
+# Print the sliced sublist containing every second element, starting from index 1
+print(sublist) 
+
+# Slice the list to get the elements in reverse order
+sublist = my_list[::-1]
+
+# Print the sliced sublist containing the elements in reverse order
+print(sublist) 
